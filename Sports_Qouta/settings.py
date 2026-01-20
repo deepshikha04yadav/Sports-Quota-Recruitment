6cb8bc17aec1e712ba05664de51a935ec204db00
Deepshikha <148853706+deepshikha04yadav@users.noreply.github.com>
2026-01-21 01:06:42 +0530

    Set DEBUG to False and update ALLOWED_HOSTS

--- Sports_Qouta/settings.py
+++ Sports_Qouta/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dr+kyvy92g94hyub*-6)r7t(g&li$p!#ytbswdigtew8cx9_$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
